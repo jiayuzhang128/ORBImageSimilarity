cf1a74047b52c82108004960ca2603c03e838f89
jiayuzhang128 <zjy_990315@163.com>
2023-03-26 23:11:59 +0800

    add comments

--- utils.py
+++ utils.py
@@ -1,91 +1,162 @@
 import numpy as np
 import glob
 import os
-import pickle
 import cv2
 import math
 from matplotlib import pyplot as plt
 
 def makeDirs(outputPath):
+	"""makedirs for outputs, including keypoints descriptors and resize images.
+
+	Args:
+		outputPath (str): the output path.
+	"""
 	if os.path.exists(outputPath + "keypoints/"):
 		pass
 	else:
 		os.makedirs(outputPath + "keypoints/")
+
 	if os.path.exists(outputPath + "descriptors/"):
 		pass
 	else:
 		os.makedirs(outputPath + "descriptors/")
+
 	if os.path.exists(outputPath + "resize/"):
 		pass
 	else:
 		os.makedirs(outputPath + "resize/")
     
 def imageLoad(samplePath, queryPath):
-    # 读取样本图像
-    sampleImageColor = cv2.imread(samplePath, cv2.IMREAD_UNCHANGED) 
-    sampleImageColorResize = imageResize(cv2.cvtColor(sampleImageColor, cv2.COLOR_BGR2RGB))
-    sampleImageGray = cv2.cvtColor(sampleImageColor, cv2.COLOR_RGB2GRAY)
+	"""Load sample image and query images.
 
-    # 读取图库图像
-    queryFileNames = glob.glob(queryPath + r"/*")
-    queryImagesColor = []
-    queryImagesColorResize = []
-    queryImagesGray = []
-    for name in queryFileNames:
-        tempImageColor = cv2.imread(name, cv2.IMREAD_UNCHANGED)
-        tempImageColorResize = imageResize(cv2.cvtColor(tempImageColor, cv2.COLOR_BGR2RGB))
-        tempImageGray = cv2.cvtColor(tempImageColor,cv2.COLOR_RGB2GRAY)
-        queryImagesGray.append(tempImageGray)
-        queryImagesColor.append(tempImageColor)
-        queryImagesColorResize.append(tempImageColorResize)
-    numOfQuery = len(queryImagesColor)
-    return sampleImageColor, sampleImageColorResize, sampleImageGray, queryImagesColor,queryImagesColorResize, queryImagesGray, numOfQuery, queryFileNames
+	Args:
+		samplePath (str): sample image path.
+		queryPath (str): query images path.
+
+	Returns:
+		tuple: returns sample image and query images in RGB resized-RGB resized-Gray; number of query images; file name of query images.
+	"""
+    # 读取样本图像
+	sampleImageColor = cv2.imread(samplePath, cv2.IMREAD_UNCHANGED) 
+	sampleImageColorResize = imageResize(cv2.cvtColor(sampleImageColor, cv2.COLOR_BGR2RGB))
+	sampleImageGray = cv2.cvtColor(sampleImageColor, cv2.COLOR_RGB2GRAY)
+
+	# 读取图库图像
+	queryFileNames = glob.glob(queryPath + r"/*")
+	queryImagesColor = []
+	queryImagesColorResize = []
+	queryImagesGray = []
+	for name in queryFileNames:
+		tempImageColor = cv2.imread(name, cv2.IMREAD_UNCHANGED)
+		tempImageColorResize = imageResize(cv2.cvtColor(tempImageColor, cv2.COLOR_BGR2RGB))
+		tempImageGray = cv2.cvtColor(tempImageColor,cv2.COLOR_RGB2GRAY)
+		queryImagesGray.append(tempImageGray)
+		queryImagesColor.append(tempImageColor)
+		queryImagesColorResize.append(tempImageColorResize)
+	numOfQuery = len(queryImagesColor)
+	return sampleImageColor, sampleImageColorResize, sampleImageGray, queryImagesColor,queryImagesColorResize, queryImagesGray, numOfQuery, queryFileNames
 
 def detectAndMatch(detector, matcher, sampleImage, queryImages, numOfQuery):
-    # 寻找关键点和描述子
-    keyPointsSample, descriptorsSample = detector.detectAndCompute(sampleImage, None)
-    keyPointsQuery = []
-    descriptorsQuery = []
-    for image in queryImages:
-        tempKeyPoints, tempDescriptors = detector.detectAndCompute(image, None)
-        keyPointsQuery.append(tempKeyPoints)
-        descriptorsQuery.append(tempDescriptors)
+	"""Detect ORB keypoints/descriptors and match then.
 
-    # 特征点匹配与匹配点对筛选 
-    matchesList =  []
-    goodMatchesList = []
-    for i in range(numOfQuery):
-        goodMatches, matches = calculateMatches(descriptorsSample,descriptorsQuery[i], matcher)
-        goodMatchesList.append(goodMatches)
-        matchesList.append(matches)
+	Args:
+		detector (cv2.ORB_create): opencv orb detector
+		matcher (cv2.matcher): opencv keypoints matcher, BF or FLANN
+		sampleImage (ndarray): sample image 
+		queryImages (list): list of query images
+		numOfQuery (int): number of query images
 
-    return keyPointsSample, descriptorsSample, keyPointsQuery, descriptorsQuery, matchesList, goodMatchesList
+	Returns:
+		tuple: returns keypoints and descriptors of sample image and query images; matching results and filtered matching results.
+	"""
+	# detect keypoints and descriptors
+	keyPointsSample, descriptorsSample = detector.detectAndCompute(sampleImage, None)
+	keyPointsQuery = []
+	descriptorsQuery = []
+	for image in queryImages:
+		tempKeyPoints, tempDescriptors = detector.detectAndCompute(image, None)
+		keyPointsQuery.append(tempKeyPoints)
+		descriptorsQuery.append(tempDescriptors)
+
+	# keypoints matching and matching point pair filtering
+	matchesList =  []
+	goodMatchesList = []
+	for i in range(numOfQuery):
+		goodMatches, matches = calculateMatches(descriptorsSample,descriptorsQuery[i], matcher)
+		goodMatchesList.append(goodMatches)
+		matchesList.append(matches)
+
+	return keyPointsSample, descriptorsSample, keyPointsQuery, descriptorsQuery, matchesList, goodMatchesList
     
 def calculateMatches(des1,des2, matcher, ratio=0.90):
-    matches = matcher.knnMatch(np.float32(des1), np.float32(des2), k=2)
-    goodMatches = []
-    for m,n in matches:
-        if m.distance < ratio*n.distance:
-            goodMatches.append([m])
+	"""Matching keypoints by compare descriptors between two images, and filtering raw matching results.
+
+	Args:
+		des1 (cv2.descriptor): descriptors1.
+		des2 (cv2.descriptor): descriptors2.
+		matcher (cv2.matcher): opencv keypoints matcher, BF or FLANN.
+		ratio (float, optional): filtering ratio. Defaults to 0.90.
+
+	Returns:
+		tuple: returns raw matching results and filtered matching results.
+	"""
+	matches = matcher.knnMatch(np.float32(des1), np.float32(des2), k=2)
+	goodMatches = []
+	for m,n in matches:
+		if m.distance < ratio*n.distance:
+			goodMatches.append([m])
             
-    return goodMatches, matches
+	return goodMatches, matches
 
 def caculateSimilarity(goodMatches, matches):
-    similarity = len(goodMatches)/len(matches)
-    return similarity
+	"""Caculate similarity
+
+	Args:
+		goodMatches (cv2.DMatch): filtered matching results
+		matches (cv2.DMatch): raw matching results
+
+	Returns:
+		float: similarity of two inages.
+	"""
+	similarity = len(goodMatches)/len(matches)
+	return similarity
 
 def saveSimilarity(outputPath, best):
+	"""Save similarity to txt file.
+
+	Args:
+		outputPath (str): output path.
+		best (float): top similarity.
+	"""
 	np.savetxt(outputPath + "topSimilarity.txt", X=np.array([best]), fmt='%.4f')
     
-def imageResize(image):
-	heightD = 480
+def imageResize(image, height=480):
+	"""Resize image into same height.
+
+	Args:
+		image (ndarray): image.
+
+	Returns:
+		ndarray: resized image.
+	"""
+	heightD = height
 	height, width = (image.shape[0],image.shape[1])
 	aspectRatio = width/height
 	newSize = (int(heightD*aspectRatio),heightD)
 	image = cv2.resize(image,newSize,interpolation=cv2.INTER_LINEAR)
+
 	return image
 
 def imageHstack(img1, img2):
+	"""Stack image by height.
+
+	Args:
+		img1 (ndarray): image1.
+		img2 (ndarray): image2.
+
+	Returns:
+		ndarray: stacked image.
+	"""
 	h1 = img1.shape[0]
 	h2 = img2.shape[0]
 	diffLines = np.abs(h1 - h2)
@@ -95,9 +166,19 @@ def imageHstack(img1, img2):
 	else:
 		img1Extend = cv2.copyMakeBorder(img1,0,diffLines,0,0,borderType = cv2.BORDER_CONSTANT,value=[255,255,255])
 		hstack = cv2.hconcat((img1Extend,img2))
+		
 	return hstack
 
 def imageVstack(img1, img2):
+	"""Stack image by width.
+
+	Args:
+		img1 (ndarray): image1.
+		img2 (ndarray): image2.
+
+	Returns:
+		ndarray: stacked image.
+	"""
 	w1 = img1.shape[1]
 	w2 = img2.shape[1]
 	diffLines = np.abs(w1 - w2)
@@ -107,9 +188,20 @@ def imageVstack(img1, img2):
 	else:
 		img1Extend = cv2.copyMakeBorder(img1,0,0,diffLines,0,borderType = cv2.BORDER_CONSTANT,value=[255,255,255])
 		hstack = cv2.vconcat((img1Extend,img2))
+
 	return hstack
 
 def plotSampleAndQuery(images, simlilarity, numOfQuery, outputPath, figureName, isShow=False):
+	"""Visualize results.
+
+	Args:
+		images (list): images to visualize.
+		simlilarity (list): similarity of the corresponding image.
+		numOfQuery (int): number of image list(query images).
+		outputPath (str): output path.
+		figureName (str): name of visualized figure.
+		isShow (bool, optional): show figure or not. Defaults to False.
+	"""
 	assert len(images) == numOfQuery
 	assert len(simlilarity) == numOfQuery
 	imagesWithSimilarity = []
@@ -131,6 +223,19 @@ def plotSampleAndQuery(images, simlilarity, numOfQuery, outputPath, figureName, 
 		plt.show()
 
 def plotKeypoints(sampleImage, keyPointsSample, queryImages, keyPointsQuery, similarity, numOfQuery, outputPath, figureName, isShow=False):
+	"""Visualize keypoints results.
+
+	Args:
+		sampleImage (ndarray): sample image.
+		keyPointsSample (cv2.keypoints): keypoints of sample image.
+		queryImages (list): images of query images.
+		keyPointsQuery (list): keypoints of query images
+		similarity (lists): similarity of the corresponding image.
+		numOfQuery (int): number of image list(query images).
+		outputPath (str): output path.
+		figureName (str): name of visualized figure.
+		isShow (bool, optional): show figure or not. Defaults to False.
+	"""
 	keyPointsImageSample = cv2.drawKeypoints(sampleImage, keyPointsSample, outImage=None)
 	keyPointsImagesSampleQuery = []
 	for i in range(numOfQuery):
@@ -140,6 +245,20 @@ def plotKeypoints(sampleImage, keyPointsSample, queryImages, keyPointsQuery, sim
 	plotSampleAndQuery(keyPointsImagesSampleQuery, similarity, numOfQuery, outputPath, figureName, isShow=isShow)
 	
 def plotMatches(sampleImage, keyPointsSample, queryImages, keyPointsQuery, similarity, goodMatchesList, numOfQuery, outputPath, figureName, isShow=False):
+	"""Visualize matching results.
+
+	Args:
+		sampleImage (ndarray): sample image.
+		keyPointsSample (cv2.keypoints): keypoints of sample image.
+		queryImages (list): images of query images.
+		keyPointsQuery (list): keypoints of query images
+		similarity (lists): similarity of the corresponding image.
+		goodMatchesList (list): filtered matching results. 
+		numOfQuery (int): number of image list(query images).
+		outputPath (str): output path.
+		figureName (str): name of visualized figure.
+		isShow (bool, optional): show figure or not. Defaults to False.
+	"""
 	goodMatchesImages = []
 	for i in range(numOfQuery):
 		tempGoodMatchesImage = cv2.drawMatchesKnn(sampleImage, keyPointsSample, queryImages[i], keyPointsQuery[i], goodMatchesList[i], None, flags=2)
@@ -151,12 +270,20 @@ def saveKeypointsAndDescriptors(keypoints,
                                 descriptors,
                                 dName,
                                 outputPath):
-	
-	# 文件名
+	"""Save keypoints and descriptors into txt files.
+
+	Args:
+		keypoints (cv2.keypoints): keypoints.
+		kName (str): txt file name of keypoints
+		descriptors (cv2.descriptors): descriptors
+		dName (str): txt file name of descriptors.
+		outputPath (str): output path.
+	"""
+	# filename
 	filename1 = outputPath + 'keypoints\\' + kName + '.txt'
 	filename2 = outputPath + 'descriptors\\' + dName + '.txt'
 
-	# 删除一存在文件
+	# delete the existing files
 	if os.path.exists(filename1):
 		os.remove(filename1)
 
@@ -177,64 +304,12 @@ def saveKeypointsAndDescriptors(keypoints,
 		strKeyPoints.append(temp)
 		i += 1
 
-	# 保存特征点（str)
+	# save keypoints into txt file（format:str)
 	np.savetxt(fname = filename1, X = strKeyPoints, fmt='%s')
 
-	# 保存描述子（hex）
+	# save descriptors into txt file（format:hex）
 	np.savetxt(fname = filename2, X = descriptors, fmt = "%x")
 
-def serializeKeypointsAndDescriptors(keypoints,
-                                descriptors,
-								matcher,
-                                feature,
-                                flags):
-	# flags = 1: 训练图库中的图片
-	# flags = 2: 待查询图片
-	
-	# File name
-	filename1 = '/home/jiayu/desktop/project/orb/feature-detection-and-matching/src/results/keypoints/{}-with-{}-keypoints{}.pkl'.format(matcher, feature, flags)
-	filename2 = '/home/jiayu/desktop/project/orb/feature-detection-and-matching/src/results/descriptors/{}-with-{}-descriptors{}.pkl'.format(matcher, feature, flags)
-
-	# Delete a file if it exists
-	if os.path.exists(filename1):
-		os.remove(filename1)
-
-	elif os.path.exists(filename2):
-		os.remove(filename2)
-
-	tempKeyPoints = []
-	for point in keypoints:
-		temp = (point.pt, point.size, point.angle, point.response, point.octave, point.class_id)
-		tempKeyPoints.append(temp)
-	with open(filename1, 'wb') as fp:
-		pickle.dump(tempKeyPoints, fp)
-
-	with open(filename2, 'wb') as fp:
-		pickle.dump(descriptors, fp)
-
-def deserializeKeypointsAndDescriptors(keypointsPath, descriptorsPath):
-	# flags = 1: 训练图库中的图片
-	# flags = 2: 待查询图片
-	
-	keypoint = []
-	file = open(keypointsPath,'rb')
-	deserializedKeypoints = pickle.load(file)
-	file.close()
-	for point in deserializedKeypoints:
-		temp = cv2.KeyPoint(
-				x=point[0][0],
-				y=point[0][1],
-				size=point[1],
-				angle=point[2],
-				response=point[3],
-				octave=point[4],
-				class_id=point[5]
-	)
-		keypoint.append(temp)
-	file = open(descriptorsPath,'rb')
-	descriptor = pickle.load(file)
-	file.close()
-	return keypoint, descriptor
 
 def calculateMatchesStrict(des1,des2, matcher, ratio=0.7):
     matches = matcher.knnMatch(np.float32(des2), np.float32(des1), k=2)
